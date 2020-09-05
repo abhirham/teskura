dda0b4d06416232474baf35d2d46d7107ea73049
abhirham <savarav.a@gmail.com>
2020-09-05 09:13:41 -0400

    disconnecting the google sign in so that it does not auto sign in with the previously used email. code refactor.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,19 +3,25 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<void> signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-    final GoogleAuthCredential googleAuthCredential =
-        GoogleAuthProvider.credential(
-            idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+    try {
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final GoogleAuthCredential googleAuthCredential =
+          GoogleAuthProvider.credential(
+              idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
 
-    await _auth.signInWithCredential(googleAuthCredential);
+      await _auth.signInWithCredential(googleAuthCredential);
+    } catch (e) {
+      print(e);
+    }
   }
 
   Future<void> signOut() async {
+    await _googleSignIn.disconnect();
     _auth.signOut();
   }
 }
